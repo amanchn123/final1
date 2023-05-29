8292d46cd1d8ace254fefcdab327f11448361e55
amanchn123 <118670647+amanchn123@users.noreply.github.com>
2023-05-29 15:43:16 +0530

    deploy

--- bakend/index.js
+++ bakend/index.js
@@ -6,15 +6,22 @@ const usernotfound=require('./CHATTING-section/errorHandling/apinotfound')
 const register=require('./socialMedia-section/routes/AuthRoute.js')
 const verify =require('./socialMedia-section/JWT_verification')
 const app=express()
+const path=require('path')
 dotenv.config()
 con()
 app.use(cors({origin:"*"}))
 
 app.use(express.json())
+app.use(express.static(path.join(__dirname,'../social-frontend/build')))
+
+app.get("*",(req,resp)=>{
+    resp.sendFile(path.join('../social-frontend/build/index.html'))
+})
+
+
+
 
 app.use('/api',require('./CHATTING-section/useroutes/userRoute'))
-// app.use(usernotfound)
-
 app.use('/apii',require('./socialMedia-section/routes/userRoutes'))
 app.use('/apii',register)
 
